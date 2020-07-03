619891c6f220fde3084cc78ede3b7d8af25b97e2
John Williams <jodawi@gmail.com>
2020-07-03 09:18:18 -0700

    dict comprehensions

---
+++ sorteddict.py
@@ -0,0 +1,18 @@
+
+foo = ( ('a','X'), ('g','c'), ('Z','w'), ('D','E') )
+print("Initial tuple:\n", foo)
+
+bar = [ (y.lower(), x ) for x,y in foo ]
+print("Transformed:\n", bar)
+
+baz = { x:y for x,y in sorted(bar) }
+print("Dict comprehension sorted by key:\n", baz)
+
+moo = { x:y for x,y in sorted(bar, key=lambda c : c[1]) }
+print("Dict comprehension sorted by value:\n", moo)
+
+moo2 = { x:y for x,y in sorted(bar, key=lambda c : c[1].lower()) }
+print("Dict comprehension sorted by value (case insensitive):\n", moo2)
+
+
+
