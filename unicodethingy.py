57cb0a56dffa92005a36a7460e7011285772a795
John Williams <jodawi@gmail.com>
2020-07-03 19:54:15 -0700

    Playing with unicode data

---
+++ unicodethingy.py
@@ -0,0 +1,23 @@
+# Find the most common words in the names of the unicode characters,
+# and also print out every character that has the word 'L' in the name
+
+import unicodedata
+import collections 
+
+named = { unicodedata.name(chr(i))
+          for i in range(0xFFFF)
+          if unicodedata.name(chr(i),'') }
+
+counter = collections.Counter()
+for name in named:
+    counter.update(name.split())
+
+print(counter.most_common(150))
+
+Ls = { chr(i) + ' (' + str(i) + ') ' + unicodedata.name(chr(i),'')
+        for i in range(0xFFFF)
+        if unicodedata.name(chr(i),'').split().__contains__('L') }
+              
+for l in sorted(Ls):
+    print(l)
+    
