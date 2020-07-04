77a2ef5aebd5d0c98736134ed939be618dad2faa
John Williams <jodawi@gmail.com>
2020-07-04 09:43:49 -0700

    Fixed valid unicode range

--- unicodethingy.py
+++ unicodethingy.py
@@ -2,10 +2,12 @@
 # and also print out every character that has the word 'ARROW' in the name
 
 import unicodedata
-import collections 
+import collections
+
+unirange = 0x10FFFF
 
 named = { unicodedata.name(chr(i))
-          for i in range(0xFFFF)
+          for i in range(unirange)
           if unicodedata.name(chr(i),'') }
 
 counter = collections.Counter()
@@ -14,8 +16,8 @@ for name in named:
 
 print(counter.most_common(150))
 
-arrows = ['{0} 0x{1:4X} {2}'.format(chr(i), i, unicodedata.name(chr(i),''))
-    for i in range(0xFFFF)
+arrows = ['{0} U+{1:X} {2}'.format(chr(i), i, unicodedata.name(chr(i),''))
+    for i in range(unirange)
     if unicodedata.name(chr(i),'').split().__contains__('ARROW') ]
               
 for arrow in arrows:
