9dbcfc39cd1d00981b12e82a63f2ff38f734cf69
John Williams <jodawi@gmail.com>
2020-07-09 12:24:07 -0700

    tweak

--- peculiar_scoping.py
+++ peculiar_scoping.py
@@ -2,30 +2,20 @@
 a = 1
 
 def foo():
-    # a has global scope
-    b = 2
-    print(b)
+    # a has global scope implicitly
     print(a)
 
 def bar():
     # a has local scope because the function assigns to it, but
-    # it is used as a local before the assignment so is an error
-    b = 3
-    print(b)
+    # it is used as a local before the assignment so is an error:
+    # UnboundLocalError: local variable 'a' referenced before assignment
+
+    # uncomment this to get rid of the exception
+    #global a
+
     print(a)
-    a = 4
+    a = 2
 
 foo()
 bar()
 
-result = """
-2
-1
-3
-Traceback (most recent call last):
-  File "D:/GitHub/Alyriant/pie/peculiar_scoping.py", line 16, in <module>
-    bar()
-  File "D:/GitHub/Alyriant/pie/peculiar_scoping.py", line 12, in bar
-    print(a)
-UnboundLocalError: local variable 'a' referenced before assignment
-"""
