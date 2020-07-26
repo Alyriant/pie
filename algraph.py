93e044a17bbbd271c68cadca28bb75792cec2910
John Williams <jodawi@gmail.com>
2020-07-25 23:37:49 -0700

    destupided my algorithm

--- algraph.py
+++ algraph.py
@@ -24,16 +24,22 @@ class ALGraph:
             for vert in verts:
                 indegree[vert] += 1
         order = []
-        while indegree:
-            for v1 in indegree:
-                #print(v1, indegree)
-                if indegree[v1] == 0:
-                    order.append(v1)
-                    indegree.pop(v1)
-                    for v2 in self.adjacency_list[v1]:
-                        indegree[v2] -= 1
-                    break
-        return order
+        queue = []
+        for v1 in indegree:
+            if indegree[v1] == 0:
+                queue.append(v1)
+        while queue:
+            v1 = queue.pop()
+            order.append(v1)
+            indegree.pop(v1)
+            for v2 in self.adjacency_list[v1]:
+                indegree[v2] -= 1
+                if indegree[v2] == 0:
+                    queue.append(v2)
+        if indegree:
+            return ["cyclic graph, no topological ordering"]
+        else:
+            return order
     
     
 if __name__ ==  "__main__":
@@ -41,3 +47,8 @@ if __name__ ==  "__main__":
     g.addEdges([[2,3], [3,4], [1,0], [1,2], [1,3]])
     print(g.adjacency_list)
     print(g.findTopologicalOrdering())
+    
+    g.addEdges([[2,1]])
+    print(g.adjacency_list)
+    print(g.findTopologicalOrdering())
+    
