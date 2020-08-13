6565053807a02c11fea202e38277ca7be9a7df3a
John Williams <jodawi@gmail.com>
2020-08-13 13:54:05 -0700

    Create leet2020-08-13.py

---
+++ leet2020-08-13.py
@@ -0,0 +1,40 @@
+class CombinationIterator:
+
+    def __init__(self, characters: str, combinationLength: int):
+        n = len(characters)
+        r = combinationLength
+        self.combos = []
+        self.nextCombo = 0
+        combo = ""
+        self.generateCombos(0, r, n, characters, combo)
+        #print(self.combos)
+        
+    def generateCombos(self, start, r, n, characters, combo):
+        for i in range(start, n - r + 1):
+            c = combo + characters[i]
+            if r == 1:
+                self.combos.append(c)
+                #print(c)
+            else:
+                self.generateCombos(i+1, r-1, n, characters, c)         
+
+    def next(self) -> str:
+        #print("next()")
+        n = self.combos[self.nextCombo]
+        self.nextCombo += 1
+        return n
+
+    def hasNext(self) -> bool:
+        #print("hasNext()")
+        return self.nextCombo < len(self.combos)
+
+# Your CombinationIterator object will be instantiated and called as such:
+# obj = CombinationIterator(characters, combinationLength)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()
+
+if __name__ == "__main__":
+    c = CombinationIterator("abc", 2)
+    while c.hasNext():
+        print(c.next())
+        
