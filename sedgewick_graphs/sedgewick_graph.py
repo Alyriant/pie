66f47d089fda447c5b3b17567661c43327e6f7c5
John Williams <jodawi@gmail.com>
2020-08-26 12:09:18 -0700

    Added graphics, random graph, adjacency-set graph

--- sedgewick_graphs/sedgewick_graph.py
+++ sedgewick_graphs/sedgewick_graph.py
@@ -1,29 +1,66 @@
-class Graph:
-    """ Graph based on Sedgewick, 17.1 """
+from graphics import *
+import math
+import random
+
+
+class Multigraph:
+    """ Adjacency-list multigraph based on Sedgewick, 17.1 """
 
     def __init__(self, num_verts, directed):
         self.num_verts = num_verts
         self.directed = directed
         self.num_edges = 0
         self.verts = [list() for _ in range(num_verts)]
-    
+
     def num_verts(self):
         return self.num_verts
-        
+
     def num_edges(self):
         return self.num_edges
-    
+
     def insert_edge(self, edge):
         self.verts[edge.v].append(edge.w)
         self.num_edges += 1
-    
+
     def remove_edge(self, edge):
         self.verts[edge.v].remove(edge.w)
         self.num_edges -= 1
-        
+
     def get_adj_iter(self, v):
         return self.verts[v]
-        
+
+    def is_directed(self):
+        return self.directed
+
+
+class Graph:
+    """ Adjacency-set graph based on Sedgewick, 17.1 """
+
+    def __init__(self, num_verts, directed):
+        self.num_verts = num_verts
+        self.directed = directed
+        self.num_edges = 0
+        self.verts = [set() for _ in range(num_verts)]
+
+    def num_verts(self):
+        return self.num_verts
+
+    def num_edges(self):
+        return self.num_edges
+
+    def insert_edge(self, edge):
+        if edge.w not in self.verts[edge.v]:
+            self.verts[edge.v].add(edge.w)
+            self.num_edges += 1
+
+    def remove_edge(self, edge):
+        if edge.w in self.verts[edge.v]:
+            self.verts[edge.v].remove(edge.w)
+            self.num_edges -= 1
+
+    def get_adj_iter(self, v):
+        return list(self.verts[v])
+
     def is_directed(self):
         return self.directed
 
@@ -87,6 +124,41 @@ class GraphIO:
     def scan_symbols(self):
         pass
 
+    @staticmethod
+    def draw_graph(graph):
+        win = GraphWin("My Circle", 1024, 1024)
+        win.setCoords(-1.1, -1.1, 1.1, 1.1)
+
+        angle = 2 * math.pi / graph.num_verts
+        id = list(range(graph.num_verts))
+        for vert in id:
+            a = angle * vert
+            c = Circle(Point(math.cos(a), math.sin(a)), 0.01)
+            c.draw(win)
+
+        edges = GraphUtilities().get_edges(graph)
+        for edge in edges:
+            a1 = angle * edge.v
+            a2 = angle * edge.w
+            p1 = Point(math.cos(a1), math.sin(a1))
+            p2 = Point(math.cos(a2), math.sin(a2))
+            line = Line(p1, p2)
+            if graph.is_directed():
+                line.setArrow("last")
+            line.draw(win)
+
+        win.getMouse()  # Pause to view result
+        win.close()  # Close window when done
+
+    @staticmethod
+    def create_random_graph(num_verts, num_edges, directed):
+        graph = Graph(num_verts, directed)
+        while graph.num_edges < num_edges:
+            v = random.randint(0, num_verts-1)
+            w = random.randint(0, num_verts-1)
+            if v != w:
+                graph.insert_edge(Edge(v, w))
+        return graph
 
 class GraphConnectedComponents:
     """ Sedgewick 17.5, 1.3, 1.4 """
@@ -145,6 +217,17 @@ class DriverExample:
             print("0 connected to 1:", cc.are_connected(0, 1))
             print("1 connected to 5:", cc.are_connected(1, 5))
 
+            io.draw_graph(graph)
+
+    @staticmethod
+    def random_graph():
+        io = GraphIO()
+        graph = io.create_random_graph(30,40, True)
+        io.draw_graph(graph)
+
 
 if __name__ == "__main__":
-    DriverExample().main()
+    ex = DriverExample()
+    # ex.main()
+    ex.random_graph()
+    
