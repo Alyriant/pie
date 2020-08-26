35377fe5c870b07b1a056e8a54d5d60a7281c6cd
John Williams <jodawi@gmail.com>
2020-08-26 12:08:43 -0700

    Update adaptive_biotech_interview.py

--- adaptive_biotech_interview.py
+++ adaptive_biotech_interview.py
@@ -1,22 +1,22 @@
-def generate_all_permutations(s):
+def generate_all_permutations(s: str):
     permutations = set()
-    
-    def _generate_all_permutations(s, start):
+
+    def _generate_all_permutations(s: str, start: int):
         fixed = s[:start]
         for i in range(start, len(s)):
             before = s[start:i]
             after = s[i+1:]
             t = fixed + s[i] + before + after
-            if start == len(s)-1:
+            if start == len(s) - 1:
                 permutations.add(t)
             else:
                 _generate_all_permutations(t, start+1)
-                
+
     _generate_all_permutations(s, 0)
-    
+
     return permutations
-    
+
+
 if __name__ == "__main__":
     x = generate_all_permutations("aacg")
     print(sorted(list(x)))
-           
